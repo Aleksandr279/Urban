0521389a4956930ff3664884516b777066c0efe4
Aleksandr279 <slik1945@rambler.ru>
2024-09-28 11:06:40 +0300

    версия 3.4.1

---
+++ module_3/module_3_4.py
@@ -0,0 +1,12 @@
+def single_root_words_(root_word_, *other_words_):
+    same_words_ = []
+    for i in other_words_:
+        range(len(other_words_))
+        if print(root_word_.lower() in i.lower()) or print(i.lower() in root_word_.lower()):
+            same_words_.append(i)
+
+    return same_words_
+
+
+result_ = single_root_words_('Domiki','DOM', 'miKkI', 'Mik', 'Omiks', 'domikiti')
+print(result_)
