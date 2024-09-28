b4d2f4164cb0b1df819288d4aa6e4239e0cea738
Aleksandr279 <slik1945@rambler.ru>
2024-09-28 12:09:22 +0300

    версия 3.4.2

--- module_3/module_3_4.py
+++ module_3/module_3_4.py
@@ -1,13 +1,12 @@
 def single_root_words_(root_word_, *other_words_):
     same_words_ = []
     for i in other_words_:
-        range(len(other_words_))
-        if print(root_word_.lower() in i.lower()) or print(i.lower() in root_word_.lower()):
+        if root_word_.lower() in i.lower() or i.lower() in root_word_.lower():
             same_words_.append(i)
-
     return same_words_
 
 
-result_ = single_root_words_('Domiki','DOM', 'miKkI', 'Mik', 'Omiks', 'domikiti')
-print(result_)
-
+result_1 = single_root_words_('Domiki','DOM', 'miKkI', 'Mik', 'Omiks', 'domikiti')
+result_2 = single_root_words_('123','012345', '32145', '12', '2', '123')
+print(result_1)
+print(result_2)
