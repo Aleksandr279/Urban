43b62ce2bcc795f6f10700bae54094cba8ee8c51
Aleksandr279 <slik1945@rambler.ru>
2024-10-03 12:38:45 +0300

    версия 3.5.5

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -1,18 +1,13 @@
 def get_multiplied_digits(number_):
     str_number_ = str(number_)
     first_ = int(str_number_[0])
-    str_number_ = str_number_.replace("0", "1")
-    n = len(str_number_)
-    a = 1
-    if n != 0:
-        for i in str_number_[1:]:
-            a *= int(i)
+    if len(str_number_) == 1 and first_ == 0:
+        return 0
+    elif len(str_number_) > 1:
         return first_ * get_multiplied_digits(int(str_number_[1:]))
-
     else:
-        return first_
-    
+        return first_ if first_ != 0 else 1
+
 
 result_ = get_multiplied_digits(40203)
 print(result_)
-
