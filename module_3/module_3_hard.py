ce215ee1faeda802fd6f47a0ea901466d9aab0a6
Aleksandr279 <slik1945@rambler.ru>
2024-10-07 20:08:10 +0300

    модуль 3.1.1

--- module_3/module_3_hard.py
+++ module_3/module_3_hard.py
@@ -1,14 +1,57 @@
-# data_structure = [
-#     [1, 2, 3],
-#     {'a': 4, 'b': 5},
-#     (6, {'cube': 7, 'drum': 8}),
-#     "Hello",
-#     ((), [{(2, 'Urban', ('Urban2', 35))}])
-# ]
+data_structure = [
+                [1, 2, 3],
+                {'a': 4, 'b': 5},
+                (6, {'cube': 7, 'drum': 8}),
+                "Hello",
+                ((), [{(2, 'Urban', ('Urban2', 35))}])
+                ]
 
 
-a = {1: 2, 2: 1}
-print(a, type(a))
-a = [*a.items()]
-print(a, type(a))
+def get_sum_values(*args):
+    global sum_data_structure
+    global list_
+    sum_data_structure = 0
+    list_ = []
+    for i in args:
+        if isinstance(i, list):
+            for j in i:
+                if isinstance(j, dict):
+                    list_.extend([*j.items()])
+                elif isinstance(j, str) or isinstance(j, int):
+                    list_.append(j)
+                elif isinstance(j, list) or isinstance(j, tuple) or isinstance(j, set):
+                    for k in j:
+                        if isinstance(k, dict):
+                            list_.extend([*k.items()])
+                        elif isinstance(k, str) or isinstance(k, int) or isinstance(k, tuple) or isinstance(k, set):
+                            list_.append(k)
+                        elif isinstance(k, list):
+                            list_.extend(k)
+    for i in list_:
+        if isinstance(i, str):
+            sum_data_structure += len(i)
+        elif isinstance(i, int):
+            sum_data_structure += i
+        else:
+            for j in i:
+                if isinstance(j, str):
+                    sum_data_structure += len(j)
+                elif isinstance(j, int):
+                    sum_data_structure += j
+                else:
+                    for k in j:
+                        if isinstance(k, str):
+                            sum_data_structure += len(k)
+                        elif isinstance(k, int):
+                            sum_data_structure += k
+                        else:
+                            for l in k:
+                                if isinstance(l, str):
+                                    sum_data_structure += len(l)
+                                elif isinstance(l, int):
+                                    sum_data_structure += l
+    return sum_data_structure
 
+
+get_sum_values(data_structure)
+print(sum_data_structure)
