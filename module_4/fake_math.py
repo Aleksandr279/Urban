5c1aafb90b8894119b7b2becd347078a312e8ecc
Aleksandr279 <slik1945@rambler.ru>
2024-10-12 16:19:16 +0300

    модуль 4.1.a

---
+++ module_4/fake_math.py
@@ -0,0 +1,16 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка '
+    else:
+        return first / second
+
+
+result_1 = divide(5, 2)
+result_2 = divide(5, 0)
+result_3 = divide(-8, 2)
+print(result_1)
+print(result_2)
+print(result_3)
+
+if __name__ == '__divide__':
+    divide()
