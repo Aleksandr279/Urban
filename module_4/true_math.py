a4fd8f013a2d5d7084ff0b4f1d0f2789e4f87c87
Aleksandr279 <slik1945@rambler.ru>
2024-10-13 19:35:25 +0300

    модуль 4.1.b

--- module_4/true_math.py
+++ module_4/true_math.py
@@ -8,12 +8,10 @@ def divide(first, second):
         return first / second
 
 
-result_1 = divide(6, 2)
-result_2 = divide(6, 0)
-result_3 = divide(-9, 2)
-print(result_1)
-print(result_2)
-print(result_3)
-
 if __name__ == "__main__":
-    divide()
+    result_1 = divide(6, 2)
+    result_2 = divide(6, 0)
+    result_3 = divide(-9, 2)
+    print(result_1)
+    print(result_2)
+    print(result_3)
