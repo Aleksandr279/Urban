57aef2488f315f88c5ff0ed2af69afa7084dd9b6
Aleksandr279 <slik1945@rambler.ru>
2024-10-13 17:53:27 +0300

    модуль 4.1.2

--- module_4/module_4_1.py
+++ module_4/module_4_1.py
@@ -1,6 +1,11 @@
 from fake_math import divide as fmd
 from true_math import divide as tmd
 
+if __name__ == "__main__":
+    fmd(69, 3)
+    fmd(3, 0)
+    tmd(49, 7)
+    tmd(15, 0)
 
 print(fmd(69, 3))
 print(fmd(3, 0))
