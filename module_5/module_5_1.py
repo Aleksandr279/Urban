69a09fd756bf5eb5fb5761fed050a83c7ca22841
Aleksandr279 <slik1945@rambler.ru>
2024-10-20 22:48:25 +0300

    модуль 5.1.1

---
+++ module_5/module_5_1.py
@@ -0,0 +1,22 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        i = 0
+        global number_of_floors
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            while i < int(new_floor):
+                print(i + 1)
+                i += 1
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
