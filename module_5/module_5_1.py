5bc50dd57e8cfb122298f633f4fee8b0c31a0c78
Aleksandr279 <slik1945@rambler.ru>
2024-10-20 22:51:07 +0300

    модуль 5.1.2

--- module_5/module_5_1.py
+++ module_5/module_5_1.py
@@ -7,7 +7,6 @@ class House:
     def go_to(self, new_floor):
         self.new_floor = new_floor
         i = 0
-        global number_of_floors
         if new_floor < 1 or new_floor > self.number_of_floors:
             print('Такого этажа не существует')
         else:
