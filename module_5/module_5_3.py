2088a63b611ea3aa439d6df90592eec36e096104
Aleksandr279 <slik1945@rambler.ru>
2024-10-26 20:15:58 +0300

    модуль 5.3.1

--- module_5/module_5_3.py
+++ module_5/module_5_3.py
@@ -23,16 +23,18 @@ class House:
         return self.number_of_floors == other.number_of_floors
 
     def __add__(self, value):
-        return self.number_of_floors + value
+        return House(self.name, self.number_of_floors + value)
+    def __radd__(self, value):
+        return House(self.name, self.number_of_floors + value)
 
-    def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
-    def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
     def __gt__(self, other):
         return self.number_of_floors > other.number_of_floors
     def __ge__(self, other):
         return self.number_of_floors >= other.number_of_floors
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
     def __ne__(self, other):
         return self.number_of_floors != other.number_of_floors
 
@@ -48,8 +50,13 @@ print(h1)
 print(h2)
 print('___________________')
 print(h1 == h2)
+h1 = h1 + 10
 print(h1)
 print(h1 == h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2
+print(h2)
 print('___________________')
 print(h1 < h2)
 print(h1 <= h2)
